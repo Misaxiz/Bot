ba3d822d8e3b472e7299d4e86e06045ed5619142
Misaxiz <misael230406@gmail.com>
2024-07-24 18:06:11 -0600

    Actividad 1.5

---
+++ Actiadad1_5.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Actiadad1_5 {
+
+    public static void main(String[] args) {
+        Scanner teclado=new Scanner(System.in);
+
+
+    System.out.println("Ingrear una palabra");
+    String palabra=teclado.nextLine(); 
+    System.out.println("ingresar letra a remplazar");
+    String remplazar=teclado.nextLine();
+    System.out.println("ingrear nueva letra");
+    String nueva=teclado.nextLine();
+    
+    System.out.println("Remplazar: " + palabra.replace(remplazar, nueva)); 
+    }
+}
